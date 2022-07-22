21d842506f7f7091121a552944c976a68d9df38e
Francisco de Melo <franciscomelojr@gmail.com>
2022-07-22 18:38:11 -0400

    COMMIT 37: xsite via upgrade

---
+++ xsite/dg828/automate_script.sh
@@ -0,0 +1,12 @@
+echo "Create DG 8 <--> DG 8 setup"
+../../automation/script_setup_cluster.sh $1 $2 $3
+oc process -f template-01-complete.yaml | oc apply -f -
+oc process -f template-02-complete.yaml | oc apply -f -
+oc process -f template-01-complete.yaml | oc apply -f -
+oc project dg-test-lon; ../../utils/script_approve_ip.sh 
+oc project dg-test-nyc; ../../utils/script_approve_ip.sh 
+oc process -f template-03-complete.yaml | oc apply -f -
+oc process -f template-04-complete.yaml | oc apply -f -
+oc process -f cluster-dg-04.yaml | oc apply -f -
+oc process -f cluster-dg-03.yaml | oc apply -f -
+oc process -f mybatch.yaml | oc apply -f -
